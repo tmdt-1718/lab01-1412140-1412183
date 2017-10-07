fcee97858d376933f163d0793328b95485024f44
1412183 <1412183PQHoang@gmail.com>
2017-10-07 17:27:54 +0700

    update model

--- app/models/category_post.rb
+++ app/models/category_post.rb
@@ -1,2 +1,3 @@
 class CategoryPost < ApplicationRecord
+    has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007055141) do
+ActiveRecord::Schema.define(version: 20171007095916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "category_posts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
