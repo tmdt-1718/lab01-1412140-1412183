a78059e0abe3681682b3b0cf84f22fe110315d4a
1412183 <1412183PQHoang@gmail.com>
2017-10-07 11:59:45 +0700

    add Post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+  before_action :find_Post, only: [:show, :edit, :update, :destroy]
+  def index
+    if params[:category].blank?
+      @Post = Post.all.order("created_at DESC")
+    else
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @Post.update(Post_params)
+      redirect_to Post_path(@Post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @Post.destroy
+    redirect_to root_path
+  end
+
+  def new
+    @Post = Post.new
+  end
+
+  def create
+    @Post = Post.new(Post_params)
+    @Post.category_id = params[:category_id]
+    if @Post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  private
+    def Post_params
+      params.require(:Post).permit(:title, :body)
+    end
+    def find_Post
+      @Post = Post.find(params[:id])
+    end
 end
