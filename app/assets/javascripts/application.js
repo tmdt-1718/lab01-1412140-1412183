3427b9c09e99b6d3260ccef7d212c6cd7b03ba5d
1412183 <1412183PQHoang@gmail.com>
2017-10-09 11:24:30 +0700

    updated manipulated hidden/show button create Post

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,7 +54,7 @@ $(document).on('click', '.panel-heading span.clickable', function(e){
 		$this.addClass('panel-collapsed');
 		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
 	} else {
-		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.parents('.panel').find('#createPost').slideDown();
 		$this.removeClass('panel-collapsed');
 		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
 	}
